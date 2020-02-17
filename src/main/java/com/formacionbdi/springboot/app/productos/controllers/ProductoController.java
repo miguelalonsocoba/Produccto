46ee9a58e3528f700c9002807f7b7b5291a05b6e
MAALONSO <miguel.coba@gnp.com.mx>
2020-02-16 21:32:53 -0600

    Se modifico la manera de usar una variable de ambiente

--- src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
@@ -7,7 +7,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,8 +26,11 @@ public class ProductoController {
 	 * Atributo del las enviroments e indica el numero de puerto en el que se
 	 * despliega la aplicación.
 	 */
-	@Autowired
-	private Environment env;
+//	@Autowired
+//	private Environment env;
+
+	@Value("${server.port}")
+	private Integer port;
 
 	/**
 	 * Variable Log.
@@ -45,7 +48,8 @@ public class ProductoController {
 	public List<Producto> listar() {
 		LOG.info("Method listar().");
 		return productoService.findAll().stream().map(producto -> {
-			producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+//			producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+			producto.setPort(port);
 			return producto;
 		}).collect(Collectors.toList());
 	}
@@ -54,7 +58,8 @@ public class ProductoController {
 	public Producto detalle(@PathVariable Long id) {
 		LOG.info("Method detalle(). Parameter-Value: " + id);
 		Producto producto = productoService.findById(id);
-		producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+//		producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		producto.setPort(port);
 		return producto;
 	}
 
